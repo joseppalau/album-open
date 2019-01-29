06e8a55f0c21ca5a549ec68afd99e397a10875b6
Administrador <administrador@iMacdeAistrador.home>
2019-01-29 13:07:17 +0100

    first touches of images frontend done

--- montlleo_album/settings.py
+++ montlleo_album/settings.py
@@ -121,4 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
