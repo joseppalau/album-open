6616637791294131ae332acb97fda3fa7761eae7
Administrador <administrador@iMacdeAistrador.home>
2019-01-31 19:34:08 +0100

    big image ok

--- album_app/static/javascript/photo_list.js
+++ album_app/static/javascript/photo_list.js
@@ -1,3 +1,4 @@
+// image selecting
 function selectImg(img){
    img_border = img.style.border;
    if(img_border == '5px solid grey'){
@@ -5,6 +6,8 @@ function selectImg(img){
    } else{
         img.style.border = '5px solid grey';
    }
+   document.getElementById('img-big').src = img.src;
+
 }
 
 
