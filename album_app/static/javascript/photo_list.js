094ee85b3b4c97d53d6034799e065f5d0489fb9a
Administrador <administrador@iMacdeAistrador.home>
2019-02-05 14:16:11 +0100

    slidehow added

--- album_app/static/javascript/photo_list.js
+++ album_app/static/javascript/photo_list.js
@@ -1,3 +1,5 @@
+// Progress Bar
+
 var num_selected = document.getElementById("num_selected").value
 var max_photos = document.getElementById("max_photos").value
 
@@ -6,8 +8,36 @@ Number(max_photos)
 
 var albumBar = document.getElementById("album-bar");
 var ctx = albumBar.getContext("2d");
-ctx.fillStyle = 'blue';
+ctx.fillStyle = 'green';
 var coverX = albumBar.width / max_photos * num_selected;
 ctx.fillRect(0,0,coverX,albumBar.height);
 
 console.log(num_selected)
+
+//Image gallery
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
