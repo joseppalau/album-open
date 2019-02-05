3d5c28a814643752b1dfe0e0f0a483b4e4f10f0c
Administrador <administrador@iMacdeAistrador.home>
2019-02-05 13:19:13 +0100

    bar progress done

--- album_app/static/javascript/photo_list.js
+++ album_app/static/javascript/photo_list.js
@@ -1,13 +1,13 @@
-// image selecting
-/*function selectImg(img){
-   img_border = img.style.border;
-   if(img_border == '5px solid grey'){
-        img.style.border = null;
-   } else{
-        img.style.border = '5px solid grey';
-   }
-   document.getElementById('img-big').src = img.src;
+var num_selected = document.getElementById("num_selected").value
+var max_photos = document.getElementById("max_photos").value
 
-}*/
+Number(num_selected)
+Number(max_photos)
 
+var albumBar = document.getElementById("album-bar");
+var ctx = albumBar.getContext("2d");
+ctx.fillStyle = 'blue';
+var coverX = albumBar.width / max_photos * num_selected;
+ctx.fillRect(0,0,coverX,albumBar.height);
 
+console.log(num_selected)
